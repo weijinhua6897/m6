9fef2a2cbadd52d78d9b0ef90e774d11816eee2c
weijinhua6897 <305884643@qq.com>
2016-06-24 16:39:05 +0800

    two

--- app/src/main/java/myappcation.java
+++ app/src/main/java/myappcation.java
@@ -12,6 +12,7 @@ public class myappcation extends Application {
     }
     {
         PlatformConfig.setWeixin("wx967daebe835fbeac", "5bb696d9ccd75a38c8a0bfe0675559b3");
+        PlatformConfig.setWeixin("wx967daebe835fbeac", "5bb696d9ccd75a38c8a0bfe0675559b3");
         //微信 appid appsecret
         PlatformConfig.setSinaWeibo("3921700954", "04b48b094faeb16683c32669824ebdad");
         //新浪微博 appkey appsecret
